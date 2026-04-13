b12879462a6f2784539361a588c8a03926ad7539
Doruk Akalin <dakalin312@gmail.com>
2026-04-13 14:36:48 -0400

    artifacts api endpoints - boilerplate

---
+++ api/backend/artifacts/artifacts_routes.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, request, jsonify, current_app
+from backend.db_connection import get_db
+
+artifacts = Blueprint("artifacts", __name__)
+
+#GET /artifacts
+
+#POST /artifacts
+
+#PUT /artifacts
+
+#DELETE /artifacts
