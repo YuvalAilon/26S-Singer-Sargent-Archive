7a7019d480df2c0b1c08773494346487f8c9f6b7
Yuval Ailon <yuval.ailon23@gmail.com>
2026-04-16 15:53:13 -0400

    projects API endpoints

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -10,6 +10,7 @@ from backend.museum_workers.museum_worker_routes import museum_workers
 from backend.donors.donors_routes import donors
 from backend.requests.requests_routes import requests
 from backend.artifact_groups.artifact_groups_routes import artifact_groups
+from backend.projects.projects_routes import projects
 
 from backend.db_connection import init_app as init_db
 from backend.simple.simple_routes import simple_routes
@@ -52,6 +53,7 @@ def create_app():
     app.register_blueprint(donors, url_prefix="/donors")
     app.register_blueprint(requests, url_prefix="/requests")
     app.register_blueprint(artifact_groups, url_prefix="/artifact_groups")
+    app.register_blueprint(projects, url_prefix="/projects")
     # Add more here if required, but we've fulfilled the 4 blueprints requirement
 
     return app
