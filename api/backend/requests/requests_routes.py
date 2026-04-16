7189199b87570b19929bd1246566f379fb6004db
Yuval Ailon <yuval.ailon23@gmail.com>
2026-04-15 22:04:23 -0400

    requests get

--- api/backend/requests/requests_routes.py
+++ api/backend/requests/requests_routes.py
@@ -1,7 +1,31 @@
 from flask import Blueprint, request, jsonify, current_app
 from backend.db_connection import get_db
+from backend.db_connection import getDBQuery
 
-artifacts = Blueprint("requests", __name__)
+requests = Blueprint("requests", __name__)
+
+@requests.route("/<int:artifact_id>", methods=["GET"])
+def get_all_requests(artifact_id):
+    return getDBQuery("SELECT * FROM ArtifactRequest WHERE requestID = " + str(artifact_id) , 'GET /<int:artifact_id>')
+
+@requests.route("/future-returns", methods=["GET"])
+def future_returns():
+    return getDBQuery("SELECT * FROM ArtifactRequest WHERE loanDateEnd > CURDATE()", 'GET /future-returns')
+
+@requests.route("/<str:before_date>", methods=["GET"])
+def future_returns_before(before_date):
+    return getDBQuery(
+        "SELECT * FROM ArtifactRequest WHERE loanDateEnd <= " + before_date,
+        "GET /<str:before_date>"
+        )
+
+@requests.route("/by-exhibit/<int:exhibit_id>", methods=["GET"])
+def get_by_exhibit(exhibit_id):
+    return getDBQuery(
+        "SELECT * FROM ArtifactRequest WHERE exhibitID = " + str(exhibit_id),
+        "GET /by-exhibit/<int:exhibit_id>"
+    )
+
 
 #GET /requests
 
